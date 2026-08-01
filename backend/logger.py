b46506891e8373c8c97e92dd58764cbde4157d24
Yuvashree R <yuvashree.ra05@gmail.com>
2026-08-01 09:09:06 +0530

    Update logger.py

--- backend/logger.py
+++ backend/logger.py
@@ -2,8 +2,7 @@
 logger.py
 ---------
 Logging middleware for the FastAPI backend.
-Logs every incoming request with logID, method, path, status code, and response time.
-
+Logs every incoming request with logID, method, path, status code, and response
 
 import time
 import logging
